b61381727390a3013f043d5b5f9daaf616a83cf5
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-20 22:18:52 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    new_array = []
+    array.each do |i|
+      if i.chars.sort == @word.chars.sort then new_array << i end
+       end 
+     new_array
+  end
+  
+end
