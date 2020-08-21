ae81b37004a2923d4ef3d594c0744192259bbf58
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-21 03:05:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,6 +11,10 @@ class Anagram
       if i.chars.sort == @word.chars.sort then new_array << i end
        end 
      new_array
+     
+     #array.select {|x| x.split("").sort == @name.split("").sort} -> would work
+
+     
   end
   
 end
