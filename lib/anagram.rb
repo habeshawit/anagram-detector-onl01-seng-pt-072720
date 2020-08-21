09f8dcb03df5d55ff8d262704e1bec481ba5fc19
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-21 03:08:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,13 +6,14 @@ class Anagram
   end
   
   def match(array)
-    new_array = []
-    array.each do |i|
-      if i.chars.sort == @word.chars.sort then new_array << i end
-       end 
-     new_array
+    #new_array = []
+    #array.each do |i|
+     # if i.chars.sort == @word.chars.sort then new_array << i end
+     #  end 
+   #  new_array
      
      #array.select {|x| x.split("").sort == @name.split("").sort} -> would work too
+     array.select{|x| x.chars.sort == @word.chars.sort}
 
      
   end
